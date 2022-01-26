1ab6f41460eceab543e1c2a25b35ac9c63fa2569
huang <tsanyu.huang@mail.utoronto.ca>
2022-01-26 03:39:40 -0500

    All tests should be working

--- Milestone 1/src/testing/AllTests.java
+++ Milestone 1/src/testing/AllTests.java
@@ -8,6 +8,7 @@ import app_kvServer.KVServer;
 import junit.framework.Test;
 import junit.framework.TestSuite;
 import logger.LogSetup;
+import shared.ServerThread;
 
 
 public class AllTests {
@@ -15,18 +16,19 @@ public class AllTests {
 	static {
 		try {
 			new LogSetup("logs/testing/test.log", Level.ERROR);
-			new KVServer(50000, 10, "FIFO");
+			new KVServer(50000, 0, null).start();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 	
-	
+
 	public static Test suite() {
 		TestSuite clientSuite = new TestSuite("Basic Storage ServerTest-Suite");
 		clientSuite.addTestSuite(ConnectionTest.class);
 		clientSuite.addTestSuite(InteractionTest.class); 
-		clientSuite.addTestSuite(AdditionalTest.class); 
+		clientSuite.addTestSuite(AdditionalTest.class);
+		clientSuite.addTestSuite(CLITest.class);
 		return clientSuite;
 	}
 	
