f95ed64b4ccfb81b876503a31fdf628fd20c2444
JhongLuo <97498110+JhongLuo@users.noreply.github.com>
2022-01-27 17:59:07 -0500

    add test for BTree

--- Milestone 1/src/testing/AllTests.java
+++ Milestone 1/src/testing/AllTests.java
@@ -28,6 +28,7 @@ public class AllTests {
 		clientSuite.addTestSuite(InteractionTest.class); 
 		clientSuite.addTestSuite(AdditionalTest.class);
 		clientSuite.addTestSuite(CLITest.class);
+		clientSuite.addTestSuite(BTreeTest.class);
 		return clientSuite;
 	}
 	
