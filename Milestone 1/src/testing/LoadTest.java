8bf6358f190f21485cb71a914ae3e2d7eb6b9076
huang <tsanyu.huang@mail.utoronto.ca>
2022-01-27 16:31:58 -0500

    Add multiple client test and one more load test

--- Milestone 1/src/testing/LoadTest.java
+++ Milestone 1/src/testing/LoadTest.java
@@ -8,6 +8,8 @@ import junit.framework.TestCase;
 import shared.messages.KVMessage;
 import shared.messages.KVMessage.StatusType;
 
+import static shared.PrintUtils.DELETE_STRING;
+
 
 public class LoadTest extends TestCase {
 
@@ -27,22 +29,59 @@ public class LoadTest extends TestCase {
 
     @Test
     /**
-     * Runs 100 puts and then gets
+     * Runs ITERATIONS of put/get
      */
-    public void test100PutGet() throws Exception {
+    public void testPutGetDeleteIntertwined() throws Exception {
         KVMessage response = null;
         Exception ex = null;
+        final int ITERATIONS = 100;
 
         int i = 0;
-        while (i < 100) {
+        while (i < ITERATIONS) {
             kvClient.put(String.valueOf(i), String.valueOf(i));
             response = kvClient.get(String.valueOf(i));
             assertTrue(
                     response.getStatus() == StatusType.GET_SUCCESS
                     && response.getValue().equals(String.valueOf(i)));
+            assertSame(kvClient.put(String.valueOf(i), DELETE_STRING).getStatus(),
+                    StatusType.DELETE_SUCCESS);
+            i += 1;
+        }
+    }
+
+
+    @Test
+    /**
+     * Runs ITERATIONS of puts and then ITERATIONS of gets
+     */
+    public void testPutThenGetThenDelete() throws Exception {
+        KVMessage response = null;
+        Exception ex = null;
+        final int ITERATIONS = 100;
+
+        int i = 0;
+        while (i < ITERATIONS) {
+            // Put in some values
+            response = kvClient.put(String.valueOf(i), String.valueOf(i));
+            assertSame(response.getStatus(), StatusType.PUT_SUCCESS);
             i += 1;
         }
 
-    }
+        i = 0;
+        while (i < ITERATIONS){
+            // Get the keys just inserted
+            response = kvClient.get(String.valueOf(i));
+            assertTrue(response.getStatus() == StatusType.GET_SUCCESS &&
+                    response.getValue().equals(String.valueOf(i)));
+            i += 1;
+        }
 
+        i = 0;
+        while (i < ITERATIONS){
+            // Delete the keys
+            assertSame(kvClient.put(String.valueOf(i), DELETE_STRING).getStatus(),
+                    StatusType.DELETE_SUCCESS);
+            i += 1;
+        }
+    }
 }
