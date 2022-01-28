475d23e7862dfaf8cf7bd996683ba13ab48eb7db
huang <tsanyu.huang@mail.utoronto.ca>
2022-01-28 00:12:18 -0500

    DO NOT MERGE

--- Milestone 1/src/testing/AllTests.java
+++ Milestone 1/src/testing/AllTests.java
@@ -27,12 +27,12 @@ public class AllTests {
 
 	public static Test suite() {
 		TestSuite clientSuite = new TestSuite("Basic Storage ServerTest-Suite");
-		clientSuite.addTestSuite(ConnectionTest.class);
-		clientSuite.addTestSuite(InteractionTest.class); 
-		clientSuite.addTestSuite(AdditionalTest.class);
-		clientSuite.addTestSuite(CLITest.class);
-		clientSuite.addTestSuite(LoadTest.class);
-		clientSuite.addTestSuite(BTreeTest.class);
+//		clientSuite.addTestSuite(ConnectionTest.class);
+//		clientSuite.addTestSuite(InteractionTest.class);
+//		clientSuite.addTestSuite(AdditionalTest.class);
+//		clientSuite.addTestSuite(CLITest.class);
+//		clientSuite.addTestSuite(LoadTest.class);
+		clientSuite.addTestSuite(PerformanceTest.class);
 		return clientSuite;
 	}
 	
