6144845370f3c9b033d038f039a89e9c0bdab65e
huang <tsanyu.huang@mail.utoronto.ca>
2022-03-03 16:41:40 -0500

    Uncomment out

--- m2/src/testing/AllTests.java
+++ m2/src/testing/AllTests.java
@@ -49,8 +49,8 @@ public class AllTests {
 		clientSuite.addTestSuite(AdditionalTest.class);
 		clientSuite.addTestSuite(CLITest.class);
 		clientSuite.addTestSuite(LoadTest.class);
-//        //Commenting out until we figure out how to test with zookeeper
-//        clientSuite.addTestSuite(ECSTest.class);
+        //Commenting out until we figure out how to test with zookeeper
+        clientSuite.addTestSuite(ECSTest.class);
         return clientSuite;
     }
 
