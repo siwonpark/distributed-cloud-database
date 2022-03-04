5a8266146f120e60ee804252d3c968fb0bce7dfa
Samuel Huang <tsanyu.huang@mail.utoronto.ca>
2022-03-04 13:03:13 -0800

    Perf test m2 (#133)
    * Perf test
    
    * patch
    
    * Turn on logging
    
    * Tune
    
    * Perf tests
    
    * Measure ECS times on startup/teardown
    
    * Fix config path
    
    * Change num ops to 1000
    
    * Add perf test
    
    * Final
    
    * Try multithreading the clients
    
    * Add parallel client
    
    * dont create ecs automatically
    
    * Add while loop
    
    * patch
    
    * change the way parallelization works
    
    * Change result string
    
    * Prepare for merge
    
    * Add comments

--- m2/src/testing/EnronPerformanceTest.java
+++ m2/src/testing/EnronPerformanceTest.java
@@ -15,6 +15,11 @@ import java.util.*;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
+/**
+ * Test performance of storage service
+ * There are a number of constants that can be adjusted to try different storage
+ * service configurations, such as number of clients, number of servers, etc.
+ */
 public class EnronPerformanceTest extends TestCase {
 
     /**
@@ -25,31 +30,45 @@ public class EnronPerformanceTest extends TestCase {
 
     public final int[] numClients = {1, 5, 20, 50, 100};
     public final int[] numServers = {1, 5, 20, 50, 100};
-    private Logger logger = Logger.getRootLogger();
+    public final int NUM_OPS = 1000;
+    public final int NUM_CLIENTS = 1;
+    public final int NUM_SERVERS = 1;
+    public final int CACHE_SIZE = 20;
+    public final String CACHE_STRATEGY = "FIFO";
+    public final int GETS_PER_PUT = 5;
 
+    private Logger logger = Logger.getRootLogger();
+    private long durationNanos = 0;
+
+
+    public void incrementDuration(long duration){
+        durationNanos += duration;
+    }
 
     private HashMap<String, String> loadEnronData(int numEntries){
         HashMap<String, String> data = new HashMap<>();
         File enronData = new File(ENRON_DATA_PATH);
         int counter = 0;
         logger.info(ENRON_DATA_PATH);
-        for (File email: enronData.listFiles()){
-            try{
-                Scanner reader = new Scanner(email);
-                while(reader.hasNextLine()){
-                    String value = reader.nextLine();
-                    if (value.length() > 500){
-                        value = value.substring(0, 500);
+        while(counter < numEntries) {
+            for (File email : enronData.listFiles()) {
+                try {
+                    Scanner reader = new Scanner(email);
+                    while (reader.hasNextLine()) {
+                        String value = reader.nextLine();
+                        if (value.length() > 500) {
+                            value = value.substring(0, 500);
+                        }
+                        data.put(Integer.toString(counter), value);
+                        counter += 1;
+                        if (counter >= numEntries) {
+                            break;
+                        }
                     }
-                    data.put(Integer.toString(counter), value);
-                    counter += 1;
-                    if (counter >= numEntries){
-                        break;
-                    }
+                } catch (Exception e) {
+                    System.out.printf("Exception at %s", email.getAbsolutePath());
+                    e.printStackTrace();
                 }
-            } catch (Exception e){
-                System.out.printf("Exception at %s", email.getAbsolutePath());
-                e.printStackTrace();
             }
         }
         return data;
@@ -60,23 +79,17 @@ public class EnronPerformanceTest extends TestCase {
         File configFile = new File("src/testing/performancetest.config");
         ECSClient ecs = new ECSClient(configFile);
         ArrayList<KVStore> clients = new ArrayList<>();
-        HashMap<String, String> enronData = loadEnronData(10500);
+        HashMap<String, String> enronData = loadEnronData(NUM_OPS + 500);
         ArrayList<String> keys = new ArrayList<>(enronData.keySet());
         ArrayList<String> putKeys = new ArrayList<>();
+        ArrayList<Thread> threads = new ArrayList<>();
         putKeys.add("AnInitialKey");
 
-        long durationNanos = 0;
         long shutdownDurationNanos = 0;
         long startupDurationNanos = 0;
-        final int NUM_OPS = 1000;
-        final int NUM_CLIENTS = 1;
-        final int NUM_SERVERS = 1;
-        final int CACHE_SIZE = 20;
-        final String CACHE_STRATEGY = "FIFO";
         int currOp = 0;
         int numPuts = 0;
         int numGets = 0;
-        final int GETS_PER_PUT = 5;
         int opsPerClient = NUM_OPS / NUM_CLIENTS;
 
         long startTime = System.nanoTime();
@@ -85,41 +98,32 @@ public class EnronPerformanceTest extends TestCase {
         ecs.start();
         startupDurationNanos = System.nanoTime() - startTime;
 
-
+        ArrayList<ParalleledClient> paralleledClients = new ArrayList<>();
         for(int i = 0 ; i < NUM_CLIENTS; i++){
             int serverToConnect = nodesAdded.get(i % nodesAdded.size()).getNodePort();
             KVStore kvClient = new KVStore("localhost", serverToConnect);
-            kvClient.connect();
-            clients.add(kvClient);
+            int keyRangeStart = i * opsPerClient;
+            int keyRangeEnd = keyRangeStart + opsPerClient;
+            List<String> clientKeys = keys.subList(keyRangeStart, keyRangeEnd);
+            ParalleledClient paralleledClient =
+                    new ParalleledClient(kvClient, this, enronData, clientKeys);
+            paralleledClients.add(paralleledClient);
         }
 
-        for(KVStore client: clients) {
-            for (int i = 0; i < opsPerClient; i++){
-                String key = keys.get(currOp);
-                long duration;
-                if (currOp % GETS_PER_PUT == 0) {
-                    String value = enronData.get(key);
-                    startTime = System.nanoTime();
-                    client.put(key, value);
-                    duration = System.nanoTime() - startTime;
-                    numPuts += 1;
-                    putKeys.add(key);
-                } else {
-                    String keyToGet = putKeys.get(numGets % putKeys.size());
-                    startTime = System.nanoTime();
-                    client.get(keyToGet);
-                    duration = System.nanoTime() - startTime;
-                    numGets += 1;
-                }
-                durationNanos += duration;
-                currOp += 1;
-            }
+        for (ParalleledClient client: paralleledClients){
+            Thread clientThread = new Thread(client);
+            threads.add(clientThread);
+            clientThread.start();
+        }
+
+        for(Thread clientThread: threads){
+            clientThread.join();
         }
 
         long durationMillis = TimeUnit.NANOSECONDS.toMillis(durationNanos);
         String result = String.format("The server did %d iterations (%d puts, %d gets) in %d milliseconds" +
-                        "This is a latency of %d ms per operation and a throughput of %f operations/s",
-                currOp, numPuts, numGets, durationMillis, durationMillis / currOp, currOp / (float)durationMillis * 1000);
+                        "This is a latency of %d ms per operation",
+                NUM_OPS + GETS_PER_PUT * NUM_OPS , NUM_OPS, GETS_PER_PUT * NUM_OPS, durationMillis, durationMillis / NUM_OPS);
         logger.info(result);
 
         startTime = System.nanoTime();
