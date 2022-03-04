d8c08013a01ee6a217f9b312a44f886d012bf7ea
huang <tsanyu.huang@mail.utoronto.ca>
2022-03-04 00:42:17 -0500

    Perf test

--- m2/src/testing/AllTests.java
+++ m2/src/testing/AllTests.java
@@ -24,20 +24,20 @@ public class AllTests {
     public static ECSClient ecs;
     private static Logger logger = Logger.getRootLogger();
 
-	static {
-		try {
-            deleteDataDir();
-			/* Refresh data directory when running tests */
-			new LogSetup("logs/testing/test.log", Level.ERROR);
-            File ecsConfigFile = new File("src/testing/ecs.config");
-            ecs = new ECSClient(ecsConfigFile);
-            ecs.addNodes(2, CACHE_STRATEGY, CACHE_SIZE);
-            ecs.start();
-
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+//	static {
+//		try {
+//            deleteDataDir();
+//			/* Refresh data directory when running tests */
+//			new LogSetup("logs/testing/test.log", Level.ERROR);
+//            File ecsConfigFile = new File("src/testing/ecs.config");
+//            ecs = new ECSClient(ecsConfigFile);
+//            ecs.addNodes(2, CACHE_STRATEGY, CACHE_SIZE);
+//            ecs.start();
+//
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
+//	}
 
     // Delete the data directory if it exists
     private static void deleteDataDir() throws IOException {
@@ -52,18 +52,18 @@ public class AllTests {
 
     public static Test suite() {
         TestSuite clientSuite = new TestSuite("Basic Storage ServerTest-Suite");
-        clientSuite.addTestSuite(DataBasePutGetTest.class);
-        clientSuite.addTestSuite(CacheTest.class);
-        clientSuite.addTestSuite(DataBaseReBootTest.class);
-		clientSuite.addTestSuite(ConnectionTest.class);
-		clientSuite.addTestSuite(InteractionTest.class);
-		clientSuite.addTestSuite(AdditionalTest.class);
-		clientSuite.addTestSuite(CLITest.class);
-		clientSuite.addTestSuite(LoadTest.class);
+//        clientSuite.addTestSuite(DataBasePutGetTest.class);
+//        clientSuite.addTestSuite(CacheTest.class);
+//        clientSuite.addTestSuite(DataBaseReBootTest.class);
+//		clientSuite.addTestSuite(ConnectionTest.class);
+//		clientSuite.addTestSuite(InteractionTest.class);
+//		clientSuite.addTestSuite(AdditionalTest.class);
+//		clientSuite.addTestSuite(CLITest.class);
+//		clientSuite.addTestSuite(LoadTest.class);
 //        //Commenting out until we figure out how to test with zookeeper
 //        clientSuite.addTestSuite(ECSTest.class);
         // We *NEED* this to be the last test in the suite!!!!!
-        clientSuite.addTestSuite(ShutDownTest.class);
+        clientSuite.addTestSuite(EnronPerformanceTest.class);
 
         return clientSuite;
     }
---
+++ m2/src/testing/EnronPerformanceTest.java
@@ -0,0 +1,124 @@
+package testing;
+
+import app_kvECS.ECSClient;
+import client.KVStore;
+import ecs.ECSNode;
+import ecs.IECSNode;
+import junit.framework.TestCase;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.File;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+
+public class EnronPerformanceTest extends TestCase {
+
+    /**
+     * Location of the enron data set.
+     */
+    public final String ENRON_DATA_PATH =
+           System.getProperty("user.dir") + "/maildir/blair-l/inbox";
+
+    public final int[] numClients = {1, 5, 20, 50, 100};
+    public final int[] numServers = {1, 5, 20, 50, 100};
+
+    @Before
+    public void setUp(){
+
+    }
+
+    @After
+    public void tearDown(){
+
+    }
+
+    private HashMap<String, String> loadEnronData(int numEntries){
+        HashMap<String, String> data = new HashMap<>();
+        File enronData = new File(ENRON_DATA_PATH);
+        int counter = 0;
+        for (File email: enronData.listFiles()){
+            try{
+                Scanner reader = new Scanner(email);
+                while(reader.hasNextLine()){
+                    String value = reader.nextLine();
+                    if (value.length() > 500){
+                        value = value.substring(0, 500);
+                    }
+                    data.put(Integer.toString(counter), value);
+                    counter += 1;
+                    if (counter >= numEntries){
+                        break;
+                    }
+                }
+            } catch (Exception e){
+                System.out.printf("Exception at %s", email.getAbsolutePath());
+                e.printStackTrace();
+            }
+        }
+        return data;
+    }
+
+    @Test
+    public void testPerformance() throws Exception {
+        File configFile = new File("src/testing/ecs.config");
+        ECSClient ecs = new ECSClient(configFile);
+        ArrayList<KVStore> clients = new ArrayList<>();
+        HashMap<String, String> enronData = loadEnronData(10000);
+        ArrayList<String> keys = new ArrayList<>(enronData.keySet());
+
+        long durationNanos = 0;
+        int numOps = 0;
+        int numPuts = 0;
+        int numGets = 0;
+        int opsPerClient = 100;
+
+        ArrayList<IECSNode> nodesAdded = (ArrayList<IECSNode>) ecs.addNodes(1, "FIFO", 20);
+        ecs.start();
+
+
+        for(int i = 0 ; i < 2; i++){
+            int serverToConnect = nodesAdded.get(i % nodesAdded.size()).getNodePort();
+            KVStore kvClient = new KVStore("localhost", serverToConnect);
+            kvClient.connect();
+            clients.add(kvClient);
+        }
+
+        for(KVStore client: clients) {
+            String key = keys.get(numOps);
+
+            for (int i = 0; i < opsPerClient; i++){
+                long duration;
+                if (numOps % 2 == 0) {
+                    String value = enronData.get(key);
+                    long startTime = System.nanoTime();
+                    client.put(key, value);
+                    duration = System.nanoTime() - startTime;
+                    numPuts += 1;
+                } else {
+                    long startTime = System.nanoTime();
+                    client.get(key);
+                    duration = System.nanoTime() - startTime;
+                    numGets += 1;
+                }
+                durationNanos += duration;
+                numOps += 1;
+            }
+        }
+
+        long durationMillis = TimeUnit.NANOSECONDS.toMillis(durationNanos);
+        System.out.printf("The server did %d iterations (%d puts, %d gets) in %d milliseconds" +
+                        "This is a latency of %d ms per operation and a throughput of %f operations/s",
+                numOps, numPuts, numGets, durationMillis, durationMillis / numOps, numOps / (float)durationMillis * 1000);
+
+        ecs.shutdown();
+    }
+
+
+
+
+
+
+}
