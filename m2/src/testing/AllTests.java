6967f1d0c82f9ac360bad2a1a179f3249413d0b3
huang <tsanyu.huang@mail.utoronto.ca>
2022-03-03 21:23:52 -0500

    Make tests work again

--- m2/src/testing/AllTests.java
+++ m2/src/testing/AllTests.java
@@ -1,47 +1,55 @@
 package testing;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 
+import app_kvECS.ECSClient;
+import ecs.ECS;
+import ecs.ECSNode;
+import ecs.IECSNode;
 import org.apache.log4j.Level;
 
-import app_kvServer.KVServer;
 import junit.framework.Test;
 import junit.framework.TestSuite;
 import logger.LogSetup;
-
+import org.apache.log4j.Logger;
 
 public class AllTests {
 	public static final int PORT = 50000;
 	public static final String CACHE_STRATEGY = "FIFO";
 	public static final int CACHE_SIZE = 5;
+	public static ECSClient ecs;
+	private static Logger logger = Logger.getRootLogger();
 
-//	static {
-//		try {
-//			/* Refresh data directory when running tests */
-//			new LogSetup("logs/testing/test.log", Level.ERROR);
-//
-//
-//			new KVServer(PORT, "127.0.0.1", "1", 2, CACHE_STRATEGY,
-//					CACHE_SIZE).start();
-//
-//		} catch (IOException e) {
-//			e.printStackTrace();
-//		} catch (InterruptedException e) {
-//			e.printStackTrace();
-//		}
-//	}
-	
+	static {
+		try {
+			/* Refresh data directory when running tests */
+			new LogSetup("logs/testing/test.log", Level.ERROR);
+			File ecsConfigFile = new File("src/testing/ecs.config");
+			ecs = new ECSClient(ecsConfigFile);
+			ecs.addNodes(2, CACHE_STRATEGY, CACHE_SIZE);
+			ecs.start();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
 
 	public static Test suite() {
 		TestSuite clientSuite = new TestSuite("Basic Storage ServerTest-Suite");
-//		clientSuite.addTestSuite(ConnectionTest.class);
-//		clientSuite.addTestSuite(InteractionTest.class);
-//		clientSuite.addTestSuite(AdditionalTest.class);
-//		clientSuite.addTestSuite(CLITest.class);
-//		clientSuite.addTestSuite(LoadTest.class);
-		//Commenting out until we figure out how to test with zookeeper
-		//clientSuite.addTestSuite(ECSTest.class);
+
+		clientSuite.addTestSuite(ConnectionTest.class);
+		clientSuite.addTestSuite(InteractionTest.class);
+		clientSuite.addTestSuite(AdditionalTest.class);
+		clientSuite.addTestSuite(CLITest.class);
+		clientSuite.addTestSuite(LoadTest.class);
+//        //Commenting out until we figure out how to test with zookeeper
+//        clientSuite.addTestSuite(ECSTest.class);
+		// We *NEED* this to be the last test in the suite!!!!!
+
 		return clientSuite;
 	}
-	
+
 }
