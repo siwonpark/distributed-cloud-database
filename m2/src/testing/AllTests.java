bde7296ccea280ff5486d1d40f1b03791b473acd
Siwon Park <siwonpk@gmail.com>
2022-03-04 14:56:25 -0500

    tests

--- m2/src/testing/AllTests.java
+++ m2/src/testing/AllTests.java
@@ -23,7 +23,7 @@ public class AllTests {
 		try {
             deleteDataDir();
 			/* Refresh data directory when running tests */
-			new LogSetup("logs/testing/test.log", Level.INFO);
+			new LogSetup("logs/testing/test.log", Level.ERROR);
             File ecsConfigFile = new File("src/testing/ecs.config");
             ecs = new ECSClient(ecsConfigFile);
             ecs.addNodes(3, CACHE_STRATEGY, CACHE_SIZE);
