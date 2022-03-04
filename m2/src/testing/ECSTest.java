0ed83f7d02c1c56afff4b4b3e02ebc88c8beff37
Siwon Park <siwonpk@gmail.com>
2022-03-03 23:52:46 -0500

    hash ring test

--- m2/src/testing/ECSTest.java
+++ m2/src/testing/ECSTest.java
@@ -1,39 +1,36 @@
 package testing;
 
+import ecs.ECSNode;
 import junit.framework.TestCase;
 import org.junit.Test;
-import testing.AllTests;
+import java.util.HashSet;
+import java.util.TreeMap;
 
 
 public class ECSTest extends TestCase {
-//    @Test
-//    /**
-//     * Test that the nodes in the hash ring have the correct hash ranges set
-//     */
-//    public void testHashRing() {
-//        ECS ecs = new ECS("ecs.config");
-//        HashSet<String> visited = new HashSet<>();
-//
-//        String currHash = ecs.hashRing.firstKey();
-//
-//        while (!visited.contains(currHash)) {
-//            visited.add(currHash);
-//
-//            ECSNode node = ecs.hashRing.get(currHash);
-//            assertEquals(currHash, node.getNodeHashRange()[1]);
-//
-//            if (currHash != ecs.hashRing.firstKey()) {
-//                assertTrue(node.getNodeHashRange()[0].compareTo(node.getNodeHashRange()[1]) < 0);
-//            }
-//
-//            currHash = node.getNodeHashRange()[0];
-//        }
-//
-//        assertTrue(visited.containsAll(ecs.hashRing.keySet()));
-//    }
-    public void testAddNodes() {
-        System.out.println(AllTests.ecs.getNodes());
-        assertEquals(AllTests.ecs.getNodes(), 1);
+    @Test
+    /**
+     * Test that the nodes in the hash ring have the correct hash ranges set
+     */
+    public void testHashRing() {
+        TreeMap<String, ECSNode> hashRing = (TreeMap<String, ECSNode>) AllTests.ecs.getNodes();
+        HashSet<String> visited = new HashSet<>();
 
+        String currHash = hashRing.firstKey();
+
+        while (!visited.contains(currHash)) {
+            visited.add(currHash);
+
+            ECSNode node = hashRing.get(currHash);
+            assertEquals(currHash, node.getNodeHashRange()[1]);
+
+            if (currHash != hashRing.firstKey()) {
+                assertTrue(node.getNodeHashRange()[0].compareTo(node.getNodeHashRange()[1]) < 0);
+            }
+
+            currHash = node.getNodeHashRange()[0];
+        }
+
+        assertTrue(visited.containsAll(hashRing.keySet()));
     }
 }
