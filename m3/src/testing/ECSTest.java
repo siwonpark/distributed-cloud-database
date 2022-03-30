343122d674e831830ad5adec8f3ab406c7c4d389
Siwon Park <siwonpk@gmail.com>
2022-03-30 16:34:27 -0400

    test

--- m3/src/testing/ECSTest.java
+++ m3/src/testing/ECSTest.java
@@ -372,6 +372,9 @@ public class ECSTest extends TestCase {
             // disconnect kvClient
             kvClient.disconnect();
 
+            // wait for replication
+            sleep(5000);
+
             // kill the node
             ecs.kill(nodeToKill.getNodeName());
 
