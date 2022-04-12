0a30f59cf32c33ba49136330e333101724c0096c
huang <tsanyu.huang@mail.utoronto.ca>
2022-04-12 16:11:39 -0400

    another one

--- m4/src/testing/CLITest.java
+++ m4/src/testing/CLITest.java
@@ -1,9 +1,12 @@
 package testing;
 
+import client.KVStore;
 import junit.framework.TestCase;
 import app_kvClient.KVClient;
 
 import java.io.*;
+
+import static testing.AllTests.port;
 
 
 public class CLITest extends TestCase{
@@ -53,14 +56,38 @@ public class CLITest extends TestCase{
     /**
      * Test that a transaction cannot be started without being connected to a server
      */
-    public void testTransactionCli(){
+    public void testInitTransactionCli(){
         String cmd = "initTransaction";
-        String EXPECTED_OUTPUT = "Error! Not connected!";
+        String EXPECTED_OUTPUT = "Error! Not Connected!";
         app.handleCommand(cmd);
         String output = testOut.toString();
-        System.out.println(output);
-        assertTrue(output.contains(EXPECTED_OUTPUT));
+        assertTrue(output.startsWith(EXPECTED_OUTPUT));
     }
+
+    /**
+     * Test the error cases of the commit CLI operation
+     */
+    public void testCommitTransactionCli(){
+        String cmd = "commit";
+        String EXPECTED_OUTPUT = "Error! Not currently in a transaction!";
+        app.handleCommand(cmd);
+        String output = testOut.toString();
+        assertTrue(output.startsWith(EXPECTED_OUTPUT));
+
+        Exception ex = null;
+        try {
+            app.newConnection("localhost", port);
+            app.handleCommand(cmd);
+            String EXPECTED_OUTPUT2 = "Error! No operations to commit!";
+            String output2 = testOut.toString();
+            assertTrue(output2.startsWith(EXPECTED_OUTPUT2));
+        } catch(Exception e) {
+            ex = e;
+        }
+
+        assertNull(ex);
+    }
+
 
     /**
      * Test the CLI when inputting an invalid parameters for put
