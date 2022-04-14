2e1f4c000821e0d7ef068fc16ad03daf884c02c5
huang <tsanyu.huang@mail.utoronto.ca>
2022-04-14 12:55:56 -0400

    Remove extra log

--- m4/src/app_kvServer/ClientConnection.java
+++ m4/src/app_kvServer/ClientConnection.java
@@ -135,9 +135,6 @@ public class ClientConnection implements Runnable {
 
 		if (message.getStatus() == StatusType.COMMIT_TRANSACTION) {
 			ArrayList<Message> operations = message.getOperations();
-			for (Message op: operations){
-				logger.info(op.getMessageString());
-			}
 			Message repliesToOperations = server.handleOperations(operations);
 			logger.info("get the message from ecs and send it to client");
 			sendMessage(repliesToOperations);
